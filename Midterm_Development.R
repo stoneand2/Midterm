628f0c8747519f6bea13f0baeeeacf1d6888db3a
stoneand2 <stoneand2@users.noreply.github.com>
2016-03-23 15:10:33 -0500

    Starting development file
    Starting the development file for the midterm package.

---
+++ Midterm_Development.R
@@ -0,0 +1,14 @@
+# Andy Stone
+# Midterm
+# March 23, 2016
+
+# Setting working directory
+setwd("~/github/Midterm")
+
+# Packages to be utilized 
+library(devtools); library(roxygen2)
+
+# Creating the skeleton of the package (only run this once to start!)
+# package.skeleton(name="integrateIt")
+
+
