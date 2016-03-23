4f375f2e1734bf86fbb9f54673f12131af295290
stoneand2 <stoneand2@users.noreply.github.com>
2016-03-23 15:20:28 -0500

    Bare bones of package, trapezoid class
    Creating the bare bones of the package using package.skeleton(). First
    go at the Trapezoid class.

--- Midterm_Development.R
+++ Midterm_Development.R
@@ -11,4 +11,41 @@ library(devtools); library(roxygen2)
 # Creating the skeleton of the package (only run this once to start!)
 # package.skeleton(name="integrateIt")
 
+# devtools functions to be used during package development (run after changing any files)
+current.code <- as.package("integrateIt") # Loads your package into R
+load_all(current.code) # Loads the functions in your package
+document(current.code) # Calls roxygen to document your files
+
+# Creating first class (Trapezoid)
+# Trapezoid class
+setClass(Class="Trapezoid", 
+         slots = c(x = "numeric",
+                   y = "numeric",
+                   result = "numeric"
+         ),
+         prototype = prototype(
+           x = numeric(),
+           y = numeric(),
+           result = numeric()
+         )
+)
+
+#' @export
+setMethod("initialize", "Trapezoid", 
+          function(.Object, x, y, result){
+            .Object@x <- x
+            .Object@y <- y
+            .Object@result <- result
+            value=callNextMethod()
+            return(value)
+          }
+) 
+
+
+
+
+
+
+
+
 
