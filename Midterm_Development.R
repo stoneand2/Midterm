8d6766f134f45b262de2947cdb3aaf2707057754
stoneand2 <stoneand2@users.noreply.github.com>
2016-03-23 15:42:56 -0500

    Trapezoid rule
    Writing code for trapezoid rule.

--- Midterm_Development.R
+++ Midterm_Development.R
@@ -41,6 +41,92 @@ setMethod("initialize", "Trapezoid",
           }
 ) 
 
+# Creating second class (Simpson)
+# Simpson class
+setClass(Class="Simpson", 
+         slots = c(x = "numeric",
+                   y = "numeric",
+                   result = "numeric"
+         ),
+         prototype = prototype(
+           x = numeric(),
+           y = numeric(),
+           result = numeric()
+         )
+)
+
+#' @export
+setMethod("initialize", "Simpson", 
+          function(.Object, x, y, result){
+            .Object@x <- x
+            .Object@y <- y
+            .Object@result <- result
+            value=callNextMethod()
+            return(value)
+          }
+) 
+
+# Creating integrateIt generic and method, starting with just Trapezoidal rule 
+setGeneric(name="integrateIt",
+           def=function(x, y, a, b, rule)
+           {standardGeneric("integrateIt")}
+)
+
+
+setMethod(f="integrateIt",
+          definition=function(x, y, a, b, rule){
+            # Check to make sure rule is specified correctly
+            
+            
+            # Calculating n, number of panels, which will be x-1
+            n <- length(x) - 1
+            # Calculating h, equal to b-a/n
+            h <- (b-a) / (n)
+            # Finding index values of x that correspond to starting and ending values
+            index.a <- which(x == a)
+            index.b <- which(x == b)
+            
+            # Trapezoidal calculation
+            if(rule == "Trapezoidal"){
+              print("Trapezoidal!")
+              
+              # Calculation will differ if n=1 or if n>1
+              if(n == 1){
+                result <- h/2 * (y[index.a] + y[index.b])
+              }
+              if(n > 1){
+                result <- h/2 * (y[index.a] + y[index.b] + sum(2*y[(index.a+1):(index.b-1)]))
+              }
+              
+              
+            }
+            
+            
+            
+            
+            
+          }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
